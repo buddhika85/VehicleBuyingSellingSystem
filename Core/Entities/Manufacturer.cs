34ff40040e8a7b9ef26f7df48ee6fcec0d04cf43
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-09-27 22:04:27 +1000

    Reset for restarting

--- Core/Entities/Manufacturer.cs
+++
@@ -1,9 +0,0 @@
-namespace Core.Entities
-{
-    public class Manufacturer : BaseEntity
-    {
-        public required string Name { get; set; }
-        public required string Originated { get; set; }
-        public IEnumerable<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
-    }    
-}
